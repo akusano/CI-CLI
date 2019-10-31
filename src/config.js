299718413770697abcebfce48216da347ffe0687
Alberto Massami Kusano <alberto.kusano@hotmail.com>
2019-10-30 23:43:41 -0300

    :rocket: Building!

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "ci-awss3"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://y8yf3yyoqe.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_oxvb72h7S",
+      APP_CLIENT_ID: "3nkdu8oia3fjha92n2m8a22f6l",
+      IDENTITY_POOL_ID: "us-east-1:fbf648dd-afe2-40e2-96fe-577390edea1f"
+    }
+  };
